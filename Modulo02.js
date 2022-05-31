994669a13fdd90c13abe700be71fde3b9a41db56
João Vitor Oliveira Souza <jvosouza@hotmail.com>
2022-05-31 17:13:01 +0000

    módulo 02 aula 07 finalizada

--- Modulo02.js
+++ Modulo02.js
@@ -1,8 +1,7 @@
 // O objetivo agora é criarmos um sistema de conta-corrente e cadastro de clientes para o contratante Bytebank, de modo que seja possível manipular o saldo desses usuários e realizar operações bancárias.
-
 // O que vamos aprender: usar a palavra 'new', uso de classes, métodos e construtores, atributos estáticos e privados, assesores get e set
 
-// Modulo 02 - Aula 03 - Adicionando comportamentos
+// Modulo 02 - Aula 07 - Métodos com Retorno
 class Cliente {
   nome;
   cpf;
@@ -17,12 +16,12 @@ class ContaCorrente {
     // 'O saldo, DESSA conta corrente deve ser verificado e subtraido'
     if (this._saldo >= valor) {
       this._saldo -= valor;
+      return valor;
     }
   }
   depositar(valor) {
-    if (valor > 0) {
-      this._saldo += valor;
-    }
+    if (valor <= 0) return; // Early Return - código mais legível
+    this._saldo += valor; // Condição aceita, deposito correto!
   }
 }
 
@@ -42,10 +41,15 @@ contaCorrenteRicardo.agencia = 1001
 
 contaCorrenteRicardo.depositar(100)
 contaCorrenteRicardo.depositar(100)
-contaCorrenteRicardo.depositar(100)
-contaCorrenteRicardo.sacar(50)
+// Quero manipular o valorSacado para futuras operações
+const valorSacado = contaCorrenteRicardo.sacar(50)
+console.log(valorSacado)
 // contaCorrenteRicardo._saldo = 1000; // Consigo acessar ainda sim
 console.log(contaCorrenteRicardo)
+
+// Perguntas:
+// Um método nunca consegue acessar atributos da classe que ele pertence.
+// Errado. Um método pode e deve acessar atributos de sua classe e para isso usamos a palavra-chave this
 
 
 
